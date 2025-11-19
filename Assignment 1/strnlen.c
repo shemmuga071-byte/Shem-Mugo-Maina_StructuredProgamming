1e872946b7f93306d464fa68c57a7270a361e2a8
shemmuga071-byte <shemmuga071@gmail.com>
2025-11-19 21:49:35 +0300

    Update strnlen.c

--- Assignment 1/strnlen.c
+++ Assignment 1/strnlen.c
@@ -1,1 +1,23 @@
 
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[100]; 
+
+    printf("Enter your name: ");
+    scanf("%99s", name); 
+
+    
+    printf("You entered: %s\n", name);
+
+    // Find and display the length
+    printf("Length of the string: %lu\n", strlen(name));
+
+    return 0;
+}
+
+
+
