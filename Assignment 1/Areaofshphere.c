cc2e162879864e2e38459651552ca4704d3e9b16
shemmuga071-byte <shemmuga071@gmail.com>
2025-11-19 21:47:30 +0300

    Update Areaofshphere.c

--- Assignment 1/Areaofshphere.c
+++ Assignment 1/Areaofshphere.c
@@ -1,1 +1,18 @@
 
+
+#include <stdio.h>
+const float PI = 3.141592653589793;
+
+int main() {
+    double radius, surfaceArea;
+
+    printf("Enter the radius of the sphere: ");
+    scanf("%lf", &radius);
+
+    surfaceArea = 4 * PI * radius * radius;
+
+    printf("The surface area of the sphere is: %.2f\n", surfaceArea);
+
+    return 0;
+}
+
